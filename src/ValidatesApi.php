492abbee5f1ecd23afd3d2de534e5ee1f190f439
Desi Cochrane <desi@desicochrane.com>
2015-09-07 20:47:54 +0200

    improved trait

--- src/ValidatesApi.php
+++ src/ValidatesApi.php
@@ -1,12 +1,24 @@
 <?php
 namespace DesiCochrane\ApiTesting;
 
-use PHPUnit_Framework_Assert as Assertion;
+use PHPUnit_Framework_Assert as PHPUnit;
 use Illuminate\Support\Facades\Validator;
 
+/**
+ * Class ValidatesApi
+ * @package DesiCochrane\ApiTesting
+ */
 trait ValidatesApi
 {
 
+    /**
+     * @var string
+     */
+    protected $paginationNamespaceKey = '';
+
+    /**
+     *
+     */
     public function setup()
     {
         parent::setUp();
@@ -14,21 +26,29 @@ trait ValidatesApi
         Validator::extendImplicit('not_present', function ($attribute, $value, $parameters) {
             return is_null($value);
         });
-        Validator::extendImplicit('strict_boolean', function ($attribute, $value, $parameters) {
+        Validator::extend('strict_boolean', function ($attribute, $value, $parameters) {
             return is_bool($value);
         });
     }
 
     /**
      * @param array $rules
-     * @param string $jsonKey
+     * @param string $namespace
      * @return $this
      */
-    protected function seeCollection(array $rules, $jsonKey = 'data')
+    protected function seeCollection(array $rules, $namespace = 'data')
     {
-        foreach ($this->getValueAtKey($jsonKey) as $item) {
-            Assertion::assertTrue(is_array($item), "Valid collection not found at key `$jsonKey`");
-            static::assertValid($rules, $item);
+        // Get the collection array from the response by the namespace
+        $collection = $this->getValueAtKey($namespace);
+
+        // Assert an array is present at the given namespace
+        PHPUnit::assertTrue(
+            is_array($collection),
+            "Valid collection not found at key `$namespace`"
+        );
+
+        foreach ($collection as $item) {
+            static::assertValidItem($item, $rules);
         }
 
         return $this;
@@ -36,12 +56,12 @@ trait ValidatesApi
 
     /**
      * @param array $rules
-     * @param null $jsonKey
+     * @param null $key
      * @return $this
      */
-    protected function seeItem(array $rules, $jsonKey = null)
+    protected function seeItem(array $rules, $key = null)
     {
-        static::assertValid($rules, $this->getValueAtKey($jsonKey));
+        static::assertValidItem($this->getValueAtKey($key), $rules);
 
         return $this;
     }
@@ -49,36 +69,92 @@ trait ValidatesApi
     /**
      * @return array
      */
-    protected function responseJson()
+    protected function jsonResponseAsArray()
     {
         return json_decode($this->response->getContent(), true);
     }
 
+    /**
+     * @param $namespace
+     * @return $this
+     */
+    protected function underPaginationNamespace($namespace)
+    {
+        $this->paginationNamespaceKey = $namespace;
 
+        return $this;
+    }
+
+    /**
+     * @param $total
+     * @param string $key
+     * @return ValidatesApi
+     */
+    protected function seeTotalResults($total, $key = 'total')
+    {
+        return $this->seePagination($key, $total);
+    }
+
+    /**
+     * @param $page
+     * @param string $key
+     * @return ValidatesApi
+     */
     protected function seeCurrentPage($page, $key = 'current_page')
     {
-        $this->seeValueAtKey($key, (int) $page, "Failed asserting current page `$key` equals `$page`");
-
-        return $this;
+        return $this->seePagination($key, $page);
     }
 
-    protected function seeTotalItems($total, $key = 'total')
+    /**
+     * @param $perPage
+     * @param string $key
+     * @return ValidatesApi
+     */
+    protected function seeResultsPerPage($perPage, $key = 'per_page')
     {
-        $this->seeValueAtKey($key, (int) $total, "Failed asserting total items `$key` equals `$total`");
-
-        return $this;
+        return $this->seePagination($key, $perPage);
     }
 
-    protected function seePerPage($perPage, $key = 'per_page')
+    /**
+     * @param $perPage
+     * @param string $key
+     * @return ValidatesApi
+     */
+    protected function seeTotalPages($perPage, $key = 'last_page')
     {
-        $this->seeValueAtKey($key, (int) $perPage, "Failed asserting items per page `$key` equals `$perPage`");
-
-        return $this;
+        return $this->seePagination($key, $perPage);
     }
 
-    protected function seeValueAtKey($key, $value, $message = '')
+    /**
+     * @param $key
+     * @param $value
+     * @return ValidatesApi
+     */
+    protected function seePagination($key, $value)
     {
-        Assertion::assertEquals($this->getValueAtKey($key), $value, $message);
+        $resolvedKey = $this->resolvePaginationKey($key);
+
+        return $this->seeValueAtKey($resolvedKey, $value);
+    }
+
+    /**
+     * @param $key
+     * @return string
+     */
+    protected function resolvePaginationKey($key)
+    {
+        return implode('.', array_filter([$this->paginationNamespaceKey, $key]));
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    protected function seeValueAtKey($key, $value)
+    {
+        $valueAtKey = $this->getValueAtKey($key);
+        PHPUnit::assertEquals($valueAtKey, $value, "Failed asserting that key `$key` equals `$value`");
 
         return $this;
     }
@@ -89,30 +165,30 @@ trait ValidatesApi
      */
     protected function getValueAtKey($key = null)
     {
-        if (is_null($key)) return $this->responseJson();
+        if (is_null($key)) return $this->jsonResponseAsArray();
 
-        static::assertArrayHasFlatKey($key, $this->responseJson());
+        static::assertArrayKeyExists($this->jsonResponseAsArray(), $key);
 
-        return array_get($this->responseJson(), $key);
+        return array_get($this->jsonResponseAsArray(), $key);
     }
 
     /**
      * @param $key
      * @param $array
      */
-    protected static function assertArrayHasFlatKey($key, $array)
+    protected static function assertArrayKeyExists($array, $key)
     {
-        return Assertion::assertTrue(array_has($array, $key), "Failed asserting that key `$key` exists");
+        return PHPUnit::assertTrue(array_has($array, $key), "Failed asserting that key `$key` exists");
     }
 
     /**
      * @param array $rules
-     * @param array $resourceData
+     * @param array $item
      */
-    protected function assertValid(array $rules, array $resourceData)
+    protected static function assertValidItem($item, array $rules)
     {
-        $validator = Validator::make($resourceData, $rules);
+        $validator = Validator::make($item, $rules);
 
-        Assertion::assertTrue($validator->passes(), $validator->errors());
+        PHPUnit::assertTrue($validator->passes(), $validator->errors());
     }
 }
